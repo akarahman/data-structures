594f143976457458a3b083479243387c66c99464
akarahman <kuadilah@umich.edu>
2018-08-02 01:16:34 -0400

    some definitions

--- hash-table/my_map.h
+++ hash-table/my_map.h
@@ -2,13 +2,13 @@
 #include <iostream>
 #include <vector>
 
-template <class Key, class T, class Compare = std::hash<Key> >
+template <class Key, class T, class Hash = std::hash<Key> >
 class my_map
 {
 public:
     /* constructors and destructors */
     my_map();
-    my_map(size);
+    my_map(int size);
     ~my_map();
 
     class iterator 
@@ -23,7 +23,9 @@ public:
 
     private:
         std::pair<Key, T> *ptr;
-    }
+        friend class my_map;
+        iterator(std::pair<Key, T> *p) : ptr(p) {}
+    };
 
     /* capacity */
     bool empty();
@@ -35,12 +37,135 @@ public:
     void erase(iterator &it);
 
     /* lookup */
-    T& operator[] const;
+    T& operator[](const Key k) const;
     iterator find(Key k);
 
 private:
-    std::vector<T> v;
-    int size;
+    std::vector<std::pair<Key, T> > v;
+    std::vector<bool> deleted;
+    std::vector<bool> empty_slot;
+    int map_size;
+    int num_buckets;
 };
 
 #endif
+
+
+template <class Key, class T, class Hash>
+my_map<Key, T, Hash>::my_map()
+{
+    v.reserve(100);
+    map_size = 0;
+    num_buckets = 100;
+    deleted.reserve(100);
+    empty_slot.reserve(100);
+    for (int i = 0; i < 100; ++i)
+    {
+        deleted[i] = false;
+        empty_slot[i] = true;
+    }
+}
+
+template <class Key, class T, class Hash>
+my_map<Key, T, Hash>::my_map(int size)
+{
+    v.reserve(size);
+    this->map_size = 0;
+    num_buckets = size;
+    deleted.reserve(size);
+    empty_slot.reserve(size);
+    for (int i = 0; i < size; ++i)
+    {
+        deleted[i] = false;
+        empty_slot[i] = true;
+    }
+}
+
+template <class Key, class T, class Hash>
+my_map<Key, T, Hash>::~my_map() {}
+
+template <class Key, class T, class Hash>
+T& my_map<Key, T, Hash>::iterator::operator* () const
+{
+    assert(ptr);
+    return *ptr;
+}
+
+template <class Key, class T, class Hash>
+T* my_map<Key, T, Hash>::iterator::operator-> () const
+{
+    assert(ptr);
+    return ptr;
+}
+
+template <class Key, class T, class Hash>
+typename my_map<Key, T, Hash>::iterator& my_map<Key, T, Hash>::iterator::operator++ ()
+{
+    // TODO
+}
+
+template <class Key, class T, class Hash>
+typename my_map<Key, T, Hash>::iterator& my_map<Key, T, Hash>::iterator::operator-- ()
+{
+    // TODO
+}
+
+template <class Key, class T, class Hash>
+bool my_map<Key, T, Hash>::iterator::operator== (iterator rhs) const
+{
+    return ptr == rhs.ptr; 
+}
+
+template <class Key, class T, class Hash>
+bool my_map<Key, T, Hash>::iterator::operator!= (iterator rhs) const
+{
+    return ptr != rhs.ptr; 
+}
+
+template <class Key, class T, class Hash>
+bool my_map<Key, T, Hash>::empty()
+{
+    return map_size == 0;
+}
+
+template <class Key, class T, class Hash>
+int my_map<Key, T, Hash>::size()
+{
+    return map_size;
+}
+
+template <class Key, class T, class Hash>
+void my_map<Key, T, Hash>::clear()
+{
+    // TODO
+}
+
+template <class Key, class T, class Hash>
+typename my_map<Key, T, Hash>::iterator my_map<Key, T, Hash>::insert(std::pair<Key, T> kvpair)
+{
+    // TODO: linear probing
+    v[Hash(kvpair.first) % num_buckets] = kvpair;
+    ++map_size;
+}
+
+template <class Key, class T, class Hash>
+void my_map<Key, T, Hash>::erase(iterator &it)
+{
+    int hash = Hash(it->first) % num_buckets;
+    deleted[hash] = true;
+    empty_slot[hash] = true;
+}
+
+template <class Key, class T, class Hash>
+T& my_map<Key, T, Hash>::operator[](const Key k) const
+{
+    // TODO: probing
+    return v[Hash(k) % num_buckets].second;
+}
+
+template <class Key, class T, class Hash>
+typename my_map<Key, T, Hash>::iterator my_map<Key, T, Hash>::find(Key k)
+{
+    // TODO: probing
+    return iterator(&v[Hash(k) % num_buckets]);
+}
